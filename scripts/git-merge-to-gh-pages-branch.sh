35d0aaff10bea409bf9eb321c7b7fd4123ea0ca5
MKU <398767+tinoviena@users.noreply.github.com>
2026-04-06 08:35:44 +0000

    no post-build for gh pages build

--- scripts/git-merge-to-gh-pages-branch.sh
+++ scripts/git-merge-to-gh-pages-branch.sh
@@ -17,6 +17,10 @@ echo "Switched to gh-pages branch"
 git merge $CURRENT_BRANCH
 echo "Merged $CURRENT_BRANCH into gh-pages"
 
+# Push merged branch to origin
+git push origin gh-pages
+echo "Pushed gh-pages to origin"
+
 # Go back to original branch
 git checkout $CURRENT_BRANCH
 echo "Switched back to $CURRENT_BRANCH"
