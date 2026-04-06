1cff8d202c933523662b51c82da1594a7f0ced56
MKU <398767+tinoviena@users.noreply.github.com>
2026-04-06 08:24:11 +0000

    This one actually works, save this!

---
+++ scripts/gh-pages-copy.sh
@@ -0,0 +1,7 @@
+mkdir -p docs 
+cp index.html docs 
+sed -i 's|dist/app.js|js/app.js|g' docs/index.html
+cp -r dist docs
+mv docs/dist docs/js
+cp buildnr.txt docs/
+touch ./docs/.nojekyll
---
+++ scripts/tag-and-push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Read build number from buildnr.txt
+BUILD_NR=$(cat buildnr.txt)
+
+# Create git tag with the build number
+git tag "v$BUILD_NR"
+
+# Push the tag to remote
+git push origin "v$BUILD_NR"
